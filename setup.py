4cb8a5f70361d63ec19d0574d2c29df7470b6a90
merrychap <merrychap.c@gmail.com>
2018-03-03 23:37:08 +0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='shellen',
-    version='0.1.9',
+    version='0.1.10',
     description='Interactive environment for crafting shellcodes. Also, it just can be used as a simple assembler/disassembler',
     long_description=long_description,
     url='https://github.com/merrychap/shellen',
